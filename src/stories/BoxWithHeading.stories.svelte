89d487edc47bbf6d698b521d300c03d61fae5db9
Moritz Utcke <62291876+Letsmoe@users.noreply.github.com>
2023-09-25 14:12:51 +0600

    Create BoxWithHeading.stories.svelte

---
+++ src/stories/BoxWithHeading.stories.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import {default as BoxWithHeading} from "../components/BoxWithHeading.svelte";
+	import "../style/tailwind.css";
+</script>
+
+<Meta title="BoxWithHeading" component={BoxWithHeading} />
+
+<Template let:args>
+	<BoxWithHeading {...args}>
+		
+	</BoxWithHeading>
+</Template>
+
+<Story
+	name="Without Children"
+	args={{
+		heading: "This is the box Heading."
+	}}
+/>
+
+<Story
+	name="With Children"
+	args={{
+		heading: "This is the box Heading."
+	}}
+	
+/>
