98a719e1f89e123a59c15d1dc390f1c3c26dfed8
Jens Cornelsen <info@online-energieausweis.org>
2026-07-27 17:34:44 +0200

    Input: optionales Icon-Snippet (Feld mit führendem Symbol)
    Ersetzt die App-Muster label.input (daisyUI) und .input-wrap/.input-icon:
    das Icon sitzt absolut links im Feld, der Input bekommt pl-9. Story ergänzt.
    
    Commit von Claude (KI)

--- src/stories/Input.stories.svelte
+++ src/stories/Input.stories.svelte
@@ -40,3 +40,18 @@
 		</Field>
 	</div>
 </Story>
+
+<Story name="Mit Icon">
+	<div class="w-72">
+		<Field label="E-Mail" for="email-icon">
+			<Input id="email-icon" type="email" placeholder="nutzer@email.com">
+				{#snippet icon()}
+					<svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+						<rect width="20" height="16" x="2" y="4" rx="2" />
+						<path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+					</svg>
+				{/snippet}
+			</Input>
+		</Field>
+	</div>
+</Story>
